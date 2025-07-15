dd68a598cc8703516ec9dd41c777e31d240b9d8b
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-07-15 22:38:50 +0100

    correction 5

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,5 @@
-import UserContext from '../UserContext';
-import ProfilePage from './ProfilePage';
-
+import UserContext from "./UserContext";
+import ProfilePage from "./ProfilePage";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import UserContext from '../UserContext';  // **note ../**
+import { useContext } from "react";
+import UserContext from "./UserContext"; // **note ../**
 
 function UserProfile() {
   const userData = useContext(UserContext);
