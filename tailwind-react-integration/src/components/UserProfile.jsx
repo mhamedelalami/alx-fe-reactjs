7c34800e685a647443d855a9559dd1bbd93d3d8e
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-08-04 20:10:13 +0100

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img className="rounded-full mx-auto w-24 h-24 md:w-36 md:h-36" src="https://via.placeholder.com/150" alt="User" />
       <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-sm md:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
