787533d6c5a7f118bd9d2b111761814ca57f1f7f
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-08-18 19:56:18 +0100

    c

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/formikForm. js";
+import FormikForm from "./components/FormikForm.js";
 
 function App() {
   return (
