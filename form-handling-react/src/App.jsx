dee7b37a822f418b823f0ec4f170d16d05e67b3b
M'hamed El Alami <elalamimhamed00@gmail.com>
2025-08-18 19:30:04 +0100

    Implementing Form Handling in React with Controlled Components and Formik

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,16 @@
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+
+function App() {
+  return (
+    <div>
+      {/* Controlled form */}
+      {/* <RegistrationForm /> */}
+
+      {/* Formik form */}
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ form-handling-react/src/components/FormikForm.jsx
@@ -0,0 +1,54 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+function FormikForm() {
+  // validation schema using Yup
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
+        console.log("Formik form submitted:", values);
+        resetForm();
+      }}
+    >
+      {() => (
+        <Form>
+          <h2>User Registration (Formik)</h2>
+
+          <div>
+            <label>Username: </label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <label>Email: </label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" style={{ color: "red" }} />
+          </div>
+
+          <div>
+            <label>Password: </label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" style={{ color: "red" }} />
+          </div>
+
+          <button type="submit">Register</button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default FormikForm;
